dab17464a0d668a05928ca5a89ae4f9c9bcac478
ArushC <65872167+ArushC@users.noreply.github.com>
2020-11-19 11:40:04 -0800

    added dictionary attack functionality

--- doubleEncryption/transposition_substitution.py
+++ doubleEncryption/transposition_substitution.py
@@ -1,4 +1,4 @@
-from transposition import railfence, boxCipher, reverseEveryN, ngramTransposition
+from transposition import railfence, swagmanCipher, reverseEveryN, ngramTransposition
 from cryptanalysis.ngramFrequencyAnalysis import get_ngrams_with_frequencies, get_most_frequent_ngram_count
 from fractionation import nfid
 from substitution.manualSubDecoder import color
@@ -6,10 +6,10 @@ from transposition import horizontalVerticalCrack
 from substitution import substitutionCrack
 from transposition.manualTransDecoder import factors
 from transposition import redefence
+from miscellaneous import dictionaryAttack
 import time
-
 NGRAMS = 3 #This constant determines how possible results will be 'scored'
-#in this case, the 'best' solution will have the highest max trigram count (scoring tends to work the best w/ trigrams)
+#in this case, the 'best' solution will have the highest max trigram count
 
 best_entries = [] #a global list -- it will be assigned values in main
 bruteforced = False
@@ -34,16 +34,15 @@ H) try horizontal transposition
 V) try vertical transposition
 F) try railfence
 D) try redefence
-B) try box cipher
-O) try box cipher with reversed blocks
+S) try swagman
+O) try swagman with reversed blocks
 N) try reverse every n
 I) try nfid (transposition in blocks)
         \n""")
         print()  # padding
-        
         # input validation
-        while not character.upper() in 'RHVFDBONI':
-            character = input("Please enter 'R', 'H', 'V', 'F', 'D', 'B', 'O', 'N', or 'I': ")
+        while not character.upper() in 'RHVFDSONI':
+            character = input("Please enter 'R', 'H', 'V', 'F', 'D', 'S', 'O', 'N', or 'I': ")
 
         if character.upper() == 'R':
             reversed_msg = not reversed_msg
@@ -69,48 +68,87 @@ I) try nfid (transposition in blocks)
             print_best_redefence(best_entries)
 
         elif character.upper() in 'HV':
+
             n = input("Transpose the n-grams. n = ")
             while not (n.isnumeric() and (int(n) > 0 and int(n) < len(msg))):
                 n = input("Transpose the n-grams. n = ")
 
             n = int(n)
 
-            bruteforce = input("Bruteforce all permutations (up to size 6)? <y/n>: ")
-            while not bruteforce.upper() in 'YN':
-                bruteforce = input("Bruteforce all permutations (up to size 6)? <y/n>: ")
+            mode_hv = input("""
+B) bruteforce
+C) custom algorithm
+D) dictionary attack
+
+""")
+
+            while not mode_hv.upper() in 'BCD':
+                mode_hv = input("Please enter 'B', 'C', or 'D':")
 
             global bruteforced #this variable needs to be changed both inside and outside
-            if bruteforce.upper() == 'Y':
+
+            if mode_hv.upper() == 'B':
                 bruteforced = True
-                best_entries[:] = get_best_decryptions_horizontal_vertical(msg,
-                mode=character.upper(), n=n)
-                print() #padding
+                print()
+                bruteforce = input("Bruteforce all permutations up to size 6? (default) <y/n>: ")
+                while not bruteforce.upper() in 'YN':
+                    bruteforce = input("Bruteforce all permutations up to size 6? (default) <y/n>: ")
+
+                if bruteforce.upper() == 'Y':
+                    best_entries[:] = get_best_decryptions_horizontal_vertical(msg, mode=character.upper(), n=n)
+                else:
+                    lower, upper = prompt_lower_upper_bound(msg)
+                    best_entries[:] = get_best_decryptions_horizontal_vertical(msg,
+                    mode=character.upper(), n=n, lower_bound=lower, upper_bound=upper)
+
+                print()  # padding
                 print_best_horizontal_vertical_entries(best_entries)
 
             else:
-                bruteforced = False
-                print("The length of the message is %d." % len(msg))
-                print("Probable key lengths are: ", end='')
+
+                #give information about factors of message kength (useful if not bruteforcing)
+                print()
+                print("The length of the message is %d. Probable key lengths are: " % len(msg), end='')
                 print(*factors(len(msg)), sep=', ')
                 print()
-                key = input("Try with key = ")
-                while not (key.isnumeric() and (int(key) > 6 and int(key) < len(msg))):
-                    key = input("Please enter a key <= 6: ")
 
-                key = int(key)
-                iterations = input("Number of iterations (100-200 recommended): ")
-                while not (iterations.isnumeric() and int(iterations) > 0):
-                    iterations = input("Please enter a valid number of iterations: ")
+                global dictionary_attacked  # this variable needs to be changed both inside and outside
 
-                iterations = int(iterations)
-                best_entries[:] = horizontalVerticalCrack.decrypt(ciphertext=msg,
-                lowerBound=key, upperBound=key, num_of_times=iterations, n=n, direction=character.upper(),
-                fitness_score=lambda x: get_most_frequent_ngram_count(x, n=NGRAMS))
+                if mode_hv.upper() == 'C':
+                    key = input("Custom permutation iteration algorithm. Try with permutation size = ")
+                    while not (key.isnumeric() and (int(key) > 1 and int(key) < len(msg))):
+                        key = input("Please enter a valid permutation size: ")
 
-                horizontalVerticalCrack.print_best_solutions(best_entries) #this is the scoring method
-                print()
+                    key = int(key)
+                    iterations = input("Number of iterations (100-200 recommended): ")
+                    while not (iterations.isnumeric() and int(iterations) > 0):
+                        iterations = input("Please enter a valid number of iterations: ")
+                    print() #padding
+                    iterations = int(iterations)
+                    best_entries[:] = horizontalVerticalCrack.decrypt(ciphertext=msg, lowerBound=key, upperBound=key,
+                                      num_of_times=iterations, n=n, direction=character.upper(),
+                                      fitness_score=lambda x: get_most_frequent_ngram_count(x, n=NGRAMS))
 
-        elif character.upper() in 'BON':
+                    horizontalVerticalCrack.print_best_solutions(best_entries)  # this is the scoring method)
+                    print()
+
+                else: #dictionary attack
+
+                    dictionary_attacked = True
+
+                    lower = input("Dictionary attack! Minimum word length = ")
+                    while not (lower.isnumeric() and (int(lower) >= 1)):
+                        lower = input("Please enter a valid minimum word length: ")
+                    upper = input("Maximum word length = ")
+                    while not (upper.isnumeric() and (int(upper) >= 1) and int(upper) >= int(lower)):
+                        upper = input("Please enter a valid maximum word length: ")
+
+                    best_entries[:] = dictionaryAttack.dict_attack(msg, 'c' if character.upper() == 'V' else 'h',
+                            int(lower), int(upper), fitness=lambda x: get_most_frequent_ngram_count(x, n=NGRAMS))
+
+                    dictionaryAttack.print_best_solutions(best_entries)
+
+        elif character.upper() in 'SON':
             lowerBound, upperBound = prompt_lower_upper_bound(msg)
             print() #padding
             best_entries[:] = get_best_decryptions_other(msg=msg, mode=character.upper(), lowerBound=lowerBound,
@@ -127,11 +165,11 @@ I) try nfid (transposition in blocks)
 
 #this is the brute-force version for horizontal-vertical: if upperBound <= 6, use this
 #returns a list of the (10) best solutions
-def get_best_decryptions_horizontal_vertical(msg, mode, n=1, num_of_decryptions=10):
+def get_best_decryptions_horizontal_vertical(msg, mode, n=1, num_of_decryptions=10, lower_bound=2, upper_bound=6):
 
     decrypt = ngramTransposition.decrypt_horizontal if mode.upper() == 'H' else ngramTransposition.decrypt_vertical
     entries = []
-    permutation_list = ngramTransposition.generatePermutations(2, 6) #bruteforce is up to size 6
+    permutation_list = ngramTransposition.generatePermutations(lower_bound, upper_bound) #bruteforce is up to size 6
 
     for perm in permutation_list:
         plaintext = decrypt(msg, perm, n)
@@ -153,18 +191,16 @@ def get_best_decryptions_railfence(msg: str, num_of_decryptions=10):
         offsets = input("Try offsets? <y/n>: ")
     if offsets.upper() == 'Y':
         offsets_upper_bound = lambda x: 2 * x - 2
-        lowerBound, upperBound = prompt_lower_upper_bound(msg)
+
     else:
         offsets_upper_bound = lambda x: 1
-        #set default lower and upper bound if offsets are not being tested (better to test all than just a few)
-        lowerBound = 2
-        upperBound = len(msg)
 
+    lowerBound, upperBound = prompt_lower_upper_bound(msg)
 
     entries = []
 
     for key in range(lowerBound, upperBound + 1):
-        for offset in range(offsets_upper_bound(key)): #this is only called
+        for offset in range(offsets_upper_bound(key)):
             plaintext = railfence.decryptMessage(key, msg, offset)
             most_freq_ngram, most_freq_ngram_count = get_most_frequent_ngram_and_count(plaintext, n=NGRAMS)  # trigrams
             entries.append((most_freq_ngram_count, most_freq_ngram, key, offset, plaintext))
@@ -172,11 +208,10 @@ def get_best_decryptions_railfence(msg: str, num_of_decryptions=10):
     print() #padding
     return retrieve_best_entries(entries, num_of_decryptions)
 
-#used for encryption in blocks (NFID)
 def get_best_decryptions_nfid(msg, num_of_decryptions=10):
     #this one only checks for the FACTORS of the message length -- it doesn't make sense if the numbers do not
-    # go in evenly
-    #in doing so, this also checks for all possible box ciphers in which the key is a factor of the message length
+    #go in evenly
+    #in doing so, this also checks for all possible swagman ciphers in which the key is a factor of the message length
     entries = []
     possible_values = factors(len(msg))
 
@@ -191,19 +226,19 @@ def get_best_decryptions_nfid(msg, num_of_decryptions=10):
     return retrieve_best_entries(entries, num_of_decryptions)
 
 
-#this is used for box cipher, box cipher with reversed blocks, and reverse every n
+#this is used for swagman, swagman with reversed blocks, and reverse every n
 def get_best_decryptions_other(msg, lowerBound, upperBound, mode, number_of_decryptions=10):
 
-    if mode in 'BO':
+    if mode in 'SO':
         reversed_blocks = True if mode.upper() == 'O' else False
-        box = True
+        swagman = True
     else:
-        box = False #this means that it is a reverse every n decryption
+        swagman = False #this means that it is a reverse every n decryption
 
     entries = []
     for key in range(lowerBound, upperBound + 1):
-        if box:
-            plaintext = boxCipher.decrypt(key=key, message=msg, reversed_blocks=reversed_blocks)
+        if swagman:
+            plaintext = swagmanCipher.decrypt(key=key, message=msg, reversed_blocks=reversed_blocks)
         else:
             plaintext = reverseEveryN.decrypt(msg, key)
 
@@ -212,7 +247,6 @@ def get_best_decryptions_other(msg, lowerBound, upperBound, mode, number_of_decr
 
     return retrieve_best_entries(entries, number_of_decryptions)
 
-#get redefence decryptions
 def get_best_decryptions_redefence(msg, number_of_decryptions=10):
 
     permutations = ngramTransposition.generatePermutations(2, 6)
@@ -323,7 +357,6 @@ def prompt_lower_upper_bound(msg):
 
     return lowerBound, upperBound
 
-
 def attempt_substitution_break(scores_list, mode):
     print()
     ask = input("Try to break any of these as a simple substitution cipher <y/n>?: ")
@@ -343,7 +376,7 @@ def attempt_substitution_break(scores_list, mode):
         #determine which printout to do in the loop based on global variables and function parameters
         #not the best way to do this but I was lazy
         print()
-        if mode.upper() in 'BON':
+        if mode.upper() in 'SON':
             print_best_other(best_entries)
         elif mode.upper() == 'I':
             print_best_nfid(best_entries)
@@ -351,9 +384,12 @@ def attempt_substitution_break(scores_list, mode):
             print_best_railfence_entries(best_entries)
         elif mode.upper() == 'D':
             print_best_redefence(best_entries)
-        else:
-           print_best_horizontal_vertical_entries(best_entries) if bruteforced else \
-           horizontalVerticalCrack.print_best_solutions(best_entries)
+        elif bruteforced:
+            print_best_horizontal_vertical_entries(best_entries)
+        elif dictionary_attacked:
+            dictionaryAttack.print_best_solutions(best_entries)
+        else: #custom algorithm
+            horizontalVerticalCrack.print_best_solutions(best_entries)
 
         print()
         ask = input("Try to break any of these as a simple substitution cipher <y/n>?: ")
@@ -372,8 +408,7 @@ if __name__ == '__main__':
 #ASLYRQQVKKFZYWRFAKTLJDDKIYQVKFYIFGIQQKLIEIELOIDYVZKLADYIKEQQIDFRLEKQDKATTYVTKLEIFVILAJEKTVHSDKIEAKTKFFQGMKQDAIJATFQVGEVDAMBEYKLYOOKXFAGQOATYEAATKZAEIMIDKLTETMZKKFASYLSDKRQILEDOYYAELQIFQKADKQIYKLITDAKEKVLQKQENKRJEKQTZSDQIGYDZDSDKYAKGKWEQYAIKAIQKQEAASKFZYXMFDDIIDEWKYATDYQIFSZQEQRDRMZFAILAIFKJEZIFEADOIQYKLSEQTDAXNFTNKTLAZWKVEYYZKSNKDKDQFITAYKVAKBFJTTKIDEAAEALYKFDFTIZLAFFTEFYIOLIXLQQSKFFRFAEBQFAGSDDEJQKYQOIDJETEQADEJYEKWEQYTISKYAIFGRQFRIREKQEFLXXYIXHEITQLEBQVEKAVXKDIZMFLFYIDTEIKFKJIDAQFFTFYARLRATKYSJYFLAKLIDXLKJKKJELFMLKMIADKQJKYIIDARKZRQJYKLLIIOZQYITKFLKYVDGYTXYTNBKLKJIDXGYFTFIKRYAFGFSIJATFKNKATYAEYJQNFIFAQILTYBKGYFLISKYGFEAYVKYNWGKLYRBJFLJYQIDEYASBYAEYBTAEKERLLIKIKENVEQIDEAEILXFEBQVEKAVXKDIZMFEAYIDLFDKIFZMDZZQDFIRQQFKIEGKFLQVKVDKRIFWXKYDTQYAVYSTUYIFAIKEQYLOLKDKIKQKLJIKKVDRLKTLDKGIFIDKYJDYLIKLXWKILZYXTYLKSTKQQNIIKYIOJEKTLZQDLIGEESIAZKQLTYRKRINFZNMQOEXLFATYKTQIFXOANEILZVFESJDXKRIDDKLLGYIDKYTYVYTDTXKYLYOKIQIFLIYEQKDNNLZGIFRVQELKDKIZIFVWXXFAE|
 #ENCRYPTED VERTICALLY WITH A KEY = 8
 #KLJFQVQITKTKFTGOYEKTKELKDKIGKYSIDSQFIQFTSYKEAFLQFDEWERIXEKYETSJJEJQJTKKFITKFFLYKDTANIVFEMQKFWTFKDRKYXIIDEKXLALDTYKFGIZYRFVILYIQTLIEDAEGQEDSDODYQTZKGKMFDFAFETNEQFTDTIOFQQEJYGLLEZFDRAIKIAROIXXGFAJYBEKLYAIEXKDDZFVDYSLJIGDKTKKQQNMQODYIKODELFKFDQLZKFKEVHQQVOKAAZRQIAKRJDDIKFWTDKJKQANKAJYFTSRETEYKEHEVIKFYFLFKYKIVLKRGTAQSFYEYKLEEKYFFFLKQIIKKKITYYESRIATVDKYXIIFFLQTFYEDQKAIETIJBLTMIASAQILEQAKQAIQIFAOAXVZAKIFLXEBYDKIFFYVFLIFYXLMKRQYTNTIJYFYYVFQBIKLBMIFQEIVYYKVFIWZNLZKYEFXJRYLYKLKEAYAKIEAKDIFDAADAFOIKKLYKEKKSYKAYDYRMZADZWKITKFFLAJQAYAKQTBMFKRTDMLIKZDGIYSYALKARBSEKAEVIKIRVKAKEKKIKLKQTIAFQIFELDYYEVKASWIYKIDLELVKMKEMAKZMFEQKAVQDSAIXYARZISELKDBFLDYFFDKDISREQKDIJLAKLKDLIYYDAFAETYGLJBRLDIXDLDQKDTAQLLDLXSOJEZZYKZSGDTLQQWRQKDGOIEDYAJKFFYKAALYLYQLKQDYWQEDEZILDYTYYTVAIZIQAOIEQQXXQAIAQKYKADZYKYTFKFQNGINJYYEVFEZADKIRXUOLKLYILQKLLTOFNXKKVINNDIVZKIQYLRVTKSGQVYXTTEDIFITNEZEQAZKQEIELNKKDKAEAEKGSJQTFRIIXDTFALKJQILFLBJYKNIIKWGIAEEQQAIZZGQYYIDQTJDYLIIGRNTEIILTQIZRV|
-#BOX KEY = 17
+#SWAGMAN KEY = 17
 #RTIAEVGGAIDKVDQIZATWBYEFIILBIYKJYTGYTFIAFMZVTITYQZNISYIILFIDKKFYTDLKRZYDIFDKAELADYQKKAJDQXYAQWDELDIKQDRINQFKDTFDYAKKVTFBQKEANGQIFZENKAIBEAEVTSKKOGATIYYIXIDLYKLWKIZTIYNKAILLTJIEOMQIKYQFRIQKKIXQASHXEKLAZYRNFNQIEDFQTLFKQEIEDVKLSDQQYEDAKZIKQAKDRJNSVFOJDKIKAYKIILYKGVYKIYZFUKGLOSNAKLDKKKQEAIAMAEELEKAYDEFYKTLDEFXAIAMDIBIQYKSLEIQRYJIXYGXNRKEWVJOQTDKOTTYITYIWEITZTFFEQRXFDTLYFKKYBRBLQEKISKDTIEOXYYQFKDEIIKQYEYAYKSKKQDXQTDSSWQYZKAFITTIJKLYEVAQDKLKYILEOLTIVQFLKLHQOAAOEQDADIONKFFKDEFLMJFJQKNFAYYANEIFKAKJXKKMFGXZZQVELESTKMSYAJESEFEZDJAQOKGTDFLEZYXSESGTVBDRDIQKLLQQZKQGIRKIFTKFLEKQKEKEQAALKAFQYJKQIFJARIIFAKKYKKKQKIDYZKAYVLKNEYYZRKKJGIFITIWADMSADEZEQYREDTYMRDDGILJBEVLFYFKITTZAITILFWQYIATAFZRFDDKMYZKKYALFQIEIYQXIKYKKFIFAAXFGWOVDKJRTLKYEAFIVDIEDAADLKQGYAKLTTIIAKYREIFILLVJTNYLEDKZKXYIKSERLXYOVZFIKLAEKTKQLZKXTIQYADFFTTIQLRDKKLFFFEBREAMIVLKLYIKFJIYQLGDQFMVXKLQQSAFQFYVFKYRJEXVFRKKLKFJYAJKQFDEYELILDTFDDLFKYYDVQVYLSDKDIZSLAEILTGEQFEFLLQJYTALGEEIZFVAQKDILYQEDIR
 #REVERSE EVERY N
 #DIVYZIELEOQQLKIIYGFIQYKVFDJKDIAFTKLZFWYRQQKVKSAYLRAETKKSHKDIEJTKVVFLIAKTELITAYTVKEDQKDIRFLKIQEQLKDAYAZIEMAETAKOQTAYXKAFGLKOYOMAEBYGVVEDAJFTQQKADIFFGQMDTKAEYILKIAKKDQQLFIQYOAYEIQELDSLKDRFKSAYTEZMKDILKTAESAKIAKQQYQIAKGKWKEDSYKAYGZDDSZQDIEJQKTEQKNRVKQLKAEODIEJIZFALFIKZMAFIQEDRRFIZSQTAYDQEDKWYDFIDIZFXYMJFTTKVKKABIFATYDKDKQZYSKNKWEVYTKALZNXTFNQEDTAYQLKSDSEDJQBAFGRFAFEQQKSFLOXILETYFILZFAFDFTFIAEYLKDIAEAYXXIHEQLFXIRERKGFQRFKSAYIQETYIYJKEWQEDAEDIEJTKQQYOTAYKSAYLRRFQTFFJKDIAETKIFFLIYDIDMZFAKXVKBEVQEIEQTLIIZOQYJLKLKRRZQIYDIAKDJQKKLIMAEJFLMKLKJKLKDIXYJLFAKNTAYAJFTKGFSFIKIYRAYGTFFJKDIXNTKBLYGXTYKLVYDIYKTFQYDIEJBLFJKGYLRKVNYWFGAEYILKSYKBYGFIQTLYFNFIAEAJYQIDMZFAKXVKBEVQEIEXLFIQEDAEKVNELLKIIEAEKREABYTAYBSYQTAYVXWYKDKDIRFQLKVVEIKGFQRFQKQZFDIZFDMZFLKDIAEIYDYDILKDIYKJKDIGFLRTKLKIVKDQKLKJKLKDIQELYOFIIAKSYUTYYTKRRZAQKLEGSEIQZLDIEJTKLKIIYOQKNQILYSKTZLXYTXLKWIDIYKTLKGLYRKDIDSEDJXLIVZFOXNAETKIQFFLTAYQMEOXNIZFNAFEIZVFWKLKDIRFQVELNGZIQEDKNFIILYOYIKQXTYKLVYTYD
-
