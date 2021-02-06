bfa96f4669301fe825b8ef6efafc8919a8245513
Arush Chhatrapati <65872167+ArushC@users.noreply.github.com>
2021-02-06 14:17:15 -0800

    Update englishScore.py

--- englishDetection/englishScore.py
+++ englishDetection/englishScore.py
@@ -9,6 +9,8 @@ MIN_WORD_LENGTH = 3
 COMMON_WORDS = os.path.join(MY_PATH, '../englishDetection/commonEnglishWords.txt')
 FULL_DICTIONARY = os.path.join(MY_PATH, '../englishDetection/dictionary.txt')
 WORD_LOOKUP = os.path.join(MY_PATH, '../englishDetection/wordLookup.txt')
+#for homophonic substitution cipher (and fast substitution cipher) --> quick access to bigram frequencies file
+BIGRAM_FREQUENCIES = os.path.join(MY_PATH, '../englishDetection/english_bigram_frequencies.txt')
 
 #use this scoring method for transposition ciphers
 def english_word_score(msg : str, filename=COMMON_WORDS, min_word_length=MIN_WORD_LENGTH): #calculates the number of common english words present in the message
@@ -23,9 +25,9 @@ def english_word_score(msg : str, filename=COMMON_WORDS, min_word_length=MIN_WOR
         trie.add(word)
     return len(re.findall(trie.pattern(), message))
 
-#use this scoring method for every other type of cipher
+#use this scoring method for every other type of cipher (higher score is better)
 #returns 1 over the words score divided by chi-squared
-#higher score is better
+#note: this scoring method is relatively slow, ngram logarithmic scores have been found to work much better
 def english_word_and_frequencies_score(msg : str, filename=COMMON_WORDS, min_word_length=MIN_WORD_LENGTH):
     return english_word_score(msg, filename, min_word_length)/calculate_chi_squared(msg)
 
