a04dccd10bc96876a93d9c1c39cdebd12bb08551
Arush Chhatrapati <65872167+ArushC@users.noreply.github.com>
2021-02-23 13:30:40 -0800

    Prints intermediate solutions

--- polyalphabetic/generalPeriodicCrack.py
+++ polyalphabetic/generalPeriodicCrack.py
@@ -8,14 +8,15 @@ from substitution.substitutionCrack import switch
 import re
 
 #This is an implementation of the pseudocode given in this paper: https://eprint.iacr.org/2020/302.pdf
-#WORD OF WARNING: this takes quite a while to run. The code was originally implemented in C, but in Python it is
-#approximately 400 times slower. To combat this, decrease the value of the parameter big_limit passed into the function.
-#The downside to this is that the result will probably have less accuracy. Otherwise, just wait a couple of hours.
+#WORD OF WARNING: the entire loop will take a very long time to run. The implementation was originally done in the C
+#language, which is 45 times faster than Python. For this reason, we print intermediate results ("best solution so far")
+#The majority of the time, these intermediate results will be close enough to recover the actual message.
+#Note: accuracy is greater for higher (ciphertext length : period) ratios.
 def break_general_periodic_cipher(ciphertext, period, big_limit=None, small_limit=None, num_of_decryptions=10,
                                   fitness_file=ngramScore.TRIGRAMS):
 
     fitness = ngramScore.ngram_score(fitness_file)
-    BIG_LIMIT = big_limit if big_limit != None else round(500000 * period * period/len(ciphertext)) #decreased BIG_LIMIT by a factor of 10
+    BIG_LIMIT = big_limit if big_limit != None else round(500000 * period * period/len(ciphertext))
     SMALL_LIMIT = small_limit if small_limit != None else 1000
 
     ciphertext_parts = get_ciphertext_slices(ciphertext, period)
@@ -56,6 +57,11 @@ def break_general_periodic_cipher(ciphertext, period, big_limit=None, small_limi
                     big_count += 1
 
             print("...")
+
+        print("Best Solution So Far: " + decrypt(ciphertext, best_key))
+        print("Best Key: ")
+        for i in range(len(best_key)):
+            print("K" + str(i) + ": " + str(best_key[i]))
 
     return best_key, decrypt(ciphertext, best_key)
 
@@ -113,14 +119,14 @@ def main():
 
     ciphertext = input("Enter a periodic cipher to decode: ")
     nonletters = re.compile('[^A-Za-z]')
-    ciphertext = re.sub(nonletters, '', ciphertext)
+    ciphertext = re.sub(nonletters, '', ciphertext).upper()
     print(probableKeyLengths.print_probable_key_lengths(msg=ciphertext))
     period = int(input("Period = "))
     best_key, plaintext = break_general_periodic_cipher(ciphertext, period)
     print("Plaintext: " + str(plaintext))
     print("Decryption Key: ")
     for i in range(len(best_key)):
-        print("K" + str(i) + ": " + str(best_key[i]))
+        print("K" + str(i + 1) + ": " + str(best_key[i]))
 
 
 if __name__ == '__main__':
